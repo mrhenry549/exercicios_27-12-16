ee6af1c949bfecb766412fbe28ce8030df9ae2a7
Henrique <Henrique@192.168.250.186>
2016-12-27 14:22:53 +0000

    update Exercicio 3

--- Exercicios_27-12-16/src/parte1/Exercicio3.java
+++ Exercicios_27-12-16/src/parte1/Exercicio3.java
@@ -15,7 +15,7 @@ public class Exercicio3 {
         socios = new ArrayList<>();
         s = new Scanner(System.in);
         
-        System.out.println("Introduza o tipo de bilhete do sócio:");
+        System.out.println("Introduza o tipo do sócio (c = com camisola oficial; n = com camisola normal:");
         
         for(int i = 0; i >= 0; i++){
             ticket = s.next().charAt(0);
@@ -31,12 +31,12 @@ public class Exercicio3 {
         
         for(int i = 0; i < socios.size(); i++){
             if(socios.get(i)=='c'){
-                valorVendaCamisolas += 14.0;
+                valorVendaCamisolas += 14;
             }
         }
         
         System.out.println("Nº de pessoas que compraram ingresso: "+sumSocios);
-        System.out.println("Valor angariado na venda de bilhetes: "+valorBilheteira+"€");
         System.out.println("Valor angariado na venda de camisolas: "+valorVendaCamisolas+"€");
+        System.out.println("Valor angariado na venda de bilhetes: "+valorBilheteira+"€");
     }
 }
