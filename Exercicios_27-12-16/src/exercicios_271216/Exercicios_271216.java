dc2db6fa0a3680b98abd8e59ffcbe16bb2950660
Henrique <Henrique@192.168.250.186>
2016-12-27 14:18:25 +0000

    Exercicio 3

--- Exercicios_27-12-16/src/exercicios_271216/Exercicios_271216.java
+++ Exercicios_27-12-16/src/exercicios_271216/Exercicios_271216.java
@@ -2,12 +2,14 @@ package exercicios_271216;
 
 import parte1.Exercicio1;
 import parte1.Exercicio2;
+import parte1.Exercicio3;
 
 public class Exercicios_271216 {
 
     public static void main(String[] args) {
-        //Exercicio1 e1 = new Exercicio1();
+        Exercicio1 e1 = new Exercicio1();
         Exercicio2 e2 = new Exercicio2();
+        Exercicio3 e3 = new Exercicio3();
     }
     
 }
---
+++ Exercicios_27-12-16/src/parte1/Exercicio3.java
@@ -0,0 +1,42 @@
+package parte1;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Exercicio3 {
+    int sumSocios = 0, valorVendaCamisolas = 0;
+    double valorBilheteira = 0;
+    char ticket;
+    ArrayList<Character> socios;
+    Scanner s;
+    
+    public Exercicio3(){
+        
+        socios = new ArrayList<>();
+        s = new Scanner(System.in);
+        
+        System.out.println("Introduza o tipo de bilhete do sócio:");
+        
+        for(int i = 0; i >= 0; i++){
+            ticket = s.next().charAt(0);
+            if(ticket=='c' || ticket=='n'){
+                sumSocios++;
+                socios.add(ticket);
+                valorBilheteira += 7.5;
+            }
+            else if(ticket=='s'){
+                break;
+            }
+        }
+        
+        for(int i = 0; i < socios.size(); i++){
+            if(socios.get(i)=='c'){
+                valorVendaCamisolas += 14.0;
+            }
+        }
+        
+        System.out.println("Nº de pessoas que compraram ingresso: "+sumSocios);
+        System.out.println("Valor angariado na venda de bilhetes: "+valorBilheteira+"€");
+        System.out.println("Valor angariado na venda de camisolas: "+valorVendaCamisolas+"€");
+    }
+}
