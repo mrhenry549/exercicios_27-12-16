91440788efcf7c5a90c5b8e1199179bca2e775c6
Henrique <Henrique@192.168.250.186>
2016-12-27 15:15:10 +0000

    Exercicio 5

--- Exercicios_27-12-16/src/parte1/Exercicio5.java
+++ Exercicios_27-12-16/src/parte1/Exercicio5.java
@@ -1,14 +1,40 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package parte1;
 
-/**
- *
- * @author Henrique
- */
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Exercicio5 {
-    
+
+    int numero;
+    ArrayList<Integer> numerosPrimos;
+
+    Scanner s;
+
+    public Exercicio5() {
+
+        s = new Scanner(System.in);
+        numerosPrimos = new ArrayList<>();
+
+        System.out.println("introduza um número inteiro qualquer:");
+
+        numero = s.nextInt();
+
+        for (int i = 1; i <= numero; i++) {
+            int cont = 0;
+            for (int num = i; num >= 1; num--) {
+                if (i % num == 0) {
+                    cont = cont + 1;
+                }
+            }
+            if (cont == 2) {
+                numerosPrimos.add(i);
+            }
+        }
+
+        System.out.println("Números primos até " + numero + ":");
+
+        for (int i = 0; i < numerosPrimos.size(); i++) {
+            System.out.println(numerosPrimos.get(i));
+        }
+    }
 }
